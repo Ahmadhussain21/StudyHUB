94b3cc9bf6e58d79ab864718e8b4b3e23bd873a1
gamer <me.ahmad2121@gmail.com>
2024-02-23 23:52:39 +0530

    creating navbar

---
+++ src/components/Common/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/Logo/Logo-Full-Light.png'
+const Navbar = () => {
+  return (
+    <div className='flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700'>
+        <div className='flex w-11/12 max-w-maxContent items-center justify-between'>
+            <Link to='/'>
+                <img src={logo} width={160} height={42} loading='lazy' alt="LOGO" />
+            </Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
